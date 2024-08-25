fc9d257d65fd95063200b90b8d3b04ecb252d131
ShawnDeng-code <88145482@qq.com>
2024-08-25 09:55:59 +0800

    feat: add search btn

--- src/compoments/TodoList/index.jsx
+++ src/compoments/TodoList/index.jsx
@@ -1,53 +1,85 @@
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   fetchTodos,
   selectTodos,
   todosError,
   todosStatus,
 } from "../../state/todosSlice.js";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import TodoItem from "../TodoItem";
 
-import {List, Pagination, CircularProgress, Paper} from "@mui/material";
+import {
+  List,
+  Pagination,
+  CircularProgress,
+  Paper,
+  Autocomplete,
+  TextField,
+} from "@mui/material";
 
 function TodoList() {
   const dispatch = useDispatch();
   const status = useSelector(todosStatus);
   const error = useSelector(todosError);
-  const todos = useSelector(selectTodos);
-  const sortedTodos = [...todos].sort((a, b) => a.completed - b.completed);
+
+  const allTodos = [...useSelector(selectTodos)].sort(
+    (a, b) => a.completed - b.completed,
+  );
 
   const [page, setPage] = useState(1);
-  const totalPages = Math.ceil(todos.length / 10);
-  const startIndex = (page - 1) * 10;
-  const endIndex = startIndex + 10;
-  const displayedTodos = sortedTodos.slice(startIndex, endIndex);
-
-  const handlePageChange = (event, value) => {
-    setPage(value);
-  };
+  const [todos, setTodos] = useState(allTodos);
 
   useEffect(() => {
     if (status === "idle") {
       dispatch(fetchTodos());
     }
-  }, [status, dispatch]);
+    if (status === "succeeded") {
+      setTodos(allTodos);
+    }
+  }, [status, dispatch, allTodos]);
 
   return (
-    <Paper sx={{mx: 20, my: 5, p: 2}}>
-      {status === "loading" && <CircularProgress/>}
+    <Paper sx={{ mx: 20, my: 5, p: 2 }}>
+      {status === "loading" && <CircularProgress />}
       {status === "failed" && <p>Error: {error}</p>}
       {status === "succeeded" && (
         <>
-          <List sx={{mx: 2}}>
-            {displayedTodos.map((todo) => (
-              <TodoItem key={todo.id} todo={todo}/>
+          <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            size="small"
+            disableClearable
+            sx={{ pb: 3, px: 6 }}
+            options={allTodos.map((option) => option.todo)}
+            onInputChange={(event, newValue) => {
+              if (newValue) {
+                setTodos(
+                  allTodos.filter((todo) => todo.todo.includes(newValue)),
+                );
+              } else if (!newValue) {
+                setTodos(allTodos);
+              }
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Search input"
+                InputProps={{ ...params.InputProps, type: "search" }}
+              />
+            )}
+          />
+
+          <List sx={{ mx: 2 }}>
+            {todos.slice((page - 1) * 10, (page - 1) * 10 + 10).map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
             ))}
           </List>
-          <Pagination count={totalPages} onChange={handlePageChange}/>
+          <Pagination
+            count={Math.ceil(todos.length / 10)}
+            onChange={(_, value) => setPage(value)}
+          />
         </>
       )}
-
     </Paper>
   );
 }
